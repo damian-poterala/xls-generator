36b6e50cefd0f8e63e5b8cc838b516c8016568f0
Damian Poterała <damian.poterala97@gmail.com>
2021-05-11 20:07:38 +0200

    Bulding the main view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule           } from '@angular/platform-browser';
+import { NgModule                } from '@angular/core';
+import { HttpClientModule        } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule             } from '@angular/forms';
+import { ReactiveFormsModule     } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent     } from './app.component';
+
+import { InputTextModule   } from 'primeng/inputtext';
+import { DropdownModule    } from 'primeng/dropdown';
+import { ColorPickerModule } from 'primeng/colorpicker';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    InputTextModule,
+    DropdownModule,
+    ColorPickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
