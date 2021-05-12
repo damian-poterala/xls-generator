5d1f26ecc47f159e92a6f90820acb70e9e2d82fd
Damian Poterała <damian.poterala97@gmail.com>
2021-05-12 20:46:59 +0200

    adding angular form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component                                                                                                              } from '@angular/core';
+import { FormArray, ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder, AbstractControl, FormControlDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  form: FormGroup = new FormGroup({
+    reportName      : new FormControl(''),
+    reportFileName  : new FormControl(''),
+    reportSheetName : new FormControl(''),
+    colorHeader     : new FormControl(''),
+    borderHeader    : new FormControl(''),
+    stickyHeader    : new FormControl(''),
+  });
+
+  yesOrNo : any = [];
+  bases   : any = [];
+
+  constructor(
+
+  ) {  }
 
   ngOnInit(): void {
+    this.yesOrNo = [
+      { label: 'Yes', value: 1 }, 
+      { label: 'No' , value: 2 }
+    ];
     
+    this.bases   = [
+      { label: 'MySql Old'  , value: 1 },
+      { label: 'MySql New'  , value: 2 },
+      { label: 'Mssql'      , value: 3 },
+      { label: 'Stock Mssql', value: 4 },
+    ]
   }
+
+  addHeaders(numberOfHeaders: number) {
+    console.log('%c Number of headers: ', 'color: #338333', numberOfHeaders);
+  }
+
 }
