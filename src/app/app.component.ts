403dea0acdaf3101dac18bb02d6815d82e972462
Damian Poterała <damian.poterala97@gmail.com>
2021-05-13 11:44:36 +0200

    adding dynamic forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component                                                                                                              } from '@angular/core';
 import { FormArray, ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder, AbstractControl, FormControlDirective } from '@angular/forms';
 
+export class TemplateRowForm {
+  [key: string]: AbstractControl;
+  nameHeader = new FormControl('', Validators.required);
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   form: FormGroup = new FormGroup({
     reportName      : new FormControl(''),
@@ -14,6 +20,8 @@ export class AppComponent {
     colorHeader     : new FormControl(''),
     borderHeader    : new FormControl(''),
     stickyHeader    : new FormControl(''),
+    amountHeaders   : new FormControl(''),
+    headersName     : new FormArray([]),
   });
 
   yesOrNo : any = [];
@@ -37,8 +45,21 @@ export class AppComponent {
     ]
   }
 
-  addHeaders(numberOfHeaders: number) {
-    console.log('%c Number of headers: ', 'color: #338333', numberOfHeaders);
+  get template() {
+    return this.form.get('headersName') as FormArray;
+  }
+
+  addedHeadersName() {
+    this.template.push(new FormGroup(new TemplateRowForm()));
+  }
+
+  generateInputsWithHeadersName() {
+    console.log('%c Number of headers: ', 'color: #338333', this.form.get('amountHeaders')?.value);
+
+    for(var i = 0; i < this.form.get('amountHeaders')?.value; i++) {
+      this.addedHeadersName();
+    }
+
   }
 
 }
